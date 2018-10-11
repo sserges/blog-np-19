cccebeaf8a3ca2051f040e21e83b6800b2e1bf4f
sserges <agounkeserge@gmail.com>
2018-10-11 17:51:24 +0000

    crispy forms added to templates

---
+++ src/posts/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from pagedown.widgets import PagedownWidget
+
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    publish = forms.DateField(widget=forms.SelectDateWidget)
+    content = forms.CharField(widget=PagedownWidget(show_preview=False))
+
+    class Meta:
+        model = Post
+        fields = [
+            "title",
+            "content",
+            "image",
+            "draft",
+            "publish",
+        ]
