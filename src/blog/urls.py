74a3a3757c0c7d5664b2023ff6fc4b101d607b6a
sserges <agounkeserge@gmail.com>
2018-11-07 06:00:34 +0000

    API package created

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     url(r'^logout/$', logout_view, name='logout'),
 
     url(r'^', include("posts.urls", namespace='posts')),
+    url(r'^api/posts/', include('posts.api.urls', namespace='posts-api')),
 ]
 
 if settings.DEBUG:
