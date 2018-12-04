0180ccd913f62230c229c070b5bf2129b09140ee
sserges <agounkeserge@gmail.com>
2018-12-04 05:59:08 +0000

    Filters added to ListAPIView

--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -1,57 +1,77 @@
+from django.db.models import Q
+from rest_framework.filters import (
+    SearchFilter,
+    OrderingFilter,
+)
 from rest_framework.generics import (
-	CreateAPIView,
-	DestroyAPIView,
-	ListAPIView,
-	RetrieveAPIView,
-	RetrieveUpdateAPIView,
-	UpdateAPIView,
+    CreateAPIView,
+    DestroyAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    RetrieveUpdateAPIView,
+    UpdateAPIView,
 )
 from rest_framework.permissions import (
-	AllowAny,
-	IsAuthenticated,
-	IsAdminUser,
-	IsAuthenticatedOrReadOnly,
+    AllowAny,
+    IsAuthenticated,
+    IsAdminUser,
+    IsAuthenticatedOrReadOnly,
 )
 
 from ..models import Post
 from .serializers import (
-	PostCreateUpdateSerializer,
-	PostDetailSerializer,
-	PostListSerializer,
+    PostCreateUpdateSerializer,
+    PostDetailSerializer,
+    PostListSerializer,
 )
 from .permissions import IsOwnerOrReadOnly
 
 class PostCreateAPIView(CreateAPIView):
-	queryset = Post.objects.all()
-	serializer_class = PostCreateUpdateSerializer
-	permission_classes = [IsAuthenticated]
+    queryset = Post.objects.all()
+    serializer_class = PostCreateUpdateSerializer
+    permission_classes = [IsAuthenticated]
 
-	def perform_create(self, serializer):
-		serializer.save(user=self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class PostDetailAPIView(RetrieveAPIView):
-	queryset = Post.objects.all()
-	serializer_class = PostDetailSerializer
-	lookup_field = 'slug'
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = 'slug'
 
 
 class PostUpdateAPIView(RetrieveUpdateAPIView):
-	queryset = Post.objects.all()
-	serializer_class = PostCreateUpdateSerializer
-	permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-	lookup_field = 'slug'
+    queryset = Post.objects.all()
+    serializer_class = PostCreateUpdateSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    lookup_field = 'slug'
 
-	def perform_update(self, serializer):
-		serializer.save(user=self.request.user)
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class PostDeleteAPIView(DestroyAPIView):
-	queryset = Post.objects.all()
-	serializer_class = PostDetailSerializer
-	lookup_field = 'slug'
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = 'slug'
 
 
 class PostListAPIView(ListAPIView):
-    queryset = Post.objects.all()
     serializer_class = PostListSerializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'content', 'user__first_name']
+
+    def get_queryset(self, *args, **kwargs):
+        # queryset_list = super(PostListAPIView, self).get_queryset(*args, **kwargs)
+        queryset_list = Post.objects.all()
+        query = self.request.GET.get("q")
+        if query:
+            queryset_list = queryset_list.filter(
+                    Q(title__icontains=query)|
+                    Q(content__icontains=query)|
+                    Q(user__first_name__icontains=query) |
+                    Q(user__last_name__icontains=query)
+                    ).distinct()
+
+        return queryset_list
