73ab1103157b58e4412a1f68f94c9133e676d687
sserges <agounkeserge@gmail.com>
2018-12-05 05:43:18 +0000

    Pagination added to ListAPIView

--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -25,6 +25,7 @@ from .serializers import (
     PostListSerializer,
 )
 from .permissions import IsOwnerOrReadOnly
+from .pagination import PostLimitOffsetPagination, PostPageNumberPagination
 
 class PostCreateAPIView(CreateAPIView):
     queryset = Post.objects.all()
@@ -54,6 +55,7 @@ class PostUpdateAPIView(RetrieveUpdateAPIView):
 class PostDeleteAPIView(DestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     lookup_field = 'slug'
 
 
@@ -61,6 +63,7 @@ class PostListAPIView(ListAPIView):
     serializer_class = PostListSerializer
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['title', 'content', 'user__first_name']
+    pagination_class = PostPageNumberPagination
 
     def get_queryset(self, *args, **kwargs):
         # queryset_list = super(PostListAPIView, self).get_queryset(*args, **kwargs)
