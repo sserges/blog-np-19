6074cce72310c1a54d2f464dd3dc6fff9f6b31c2
sserges <agounkeserge@gmail.com>
2018-12-11 12:18:01 +0000

    Database backup & PostgreSQL database settings

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -85,10 +85,23 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# SQLITE
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME':  os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+#PostgreSQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog',
+        'USER': 'openpg',
+        'PASSWORD': 'openpgpwd',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
