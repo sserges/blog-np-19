0a41664de95115845a487433723e6df5ebddeef5
sserges <agounkeserge@gmail.com>
2019-01-04 01:41:52 +0000

    25 - User API Setup

---
+++ src/accounts/api/views.py
@@ -0,0 +1,38 @@
+from django.db.models import Q
+from django.contrib.auth import get_user_model
+from rest_framework.filters import (
+    SearchFilter,
+    OrderingFilter,
+)
+from rest_framework.mixins import (
+    DestroyModelMixin,
+    UpdateModelMixin,
+)
+from rest_framework.generics import (
+    CreateAPIView,
+    DestroyAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    RetrieveUpdateAPIView,
+    UpdateAPIView,
+)
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAdminUser,
+    IsAuthenticatedOrReadOnly,
+)
+
+from posts.api.permissions import IsOwnerOrReadOnly
+from posts.api.pagination import PostLimitOffsetPagination, PostPageNumberPagination
+from .serializers import (
+   UserCreateSerializer,
+)
+
+
+User = get_user_model()
+
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserCreateSerializer
+    queryset = User.objects.all()
